f391afc601f3c0a1bd19a0e01fc557a22ab21b06
borja <borja.lopez.fernandez@gmail.com>
2013-05-13 17:35:50 +0200

    code_to_be_Deleted removed from repository

--- code/code_to_be_Deleted/cuc/test/testConverters.rb
+++
@@ -1,9 +0,0 @@
-require 'cuc/Converters'
-
-include CUC::Converters
-
-strDate = "20120325T154814"
-
-aDate = self.str2date(strDate)
-
-puts aDate
--- code/code_to_be_Deleted/minarc/test/test.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-#aTime = Time.local(2050, 01, 01, 00, 00, 00)
-
-aDateTime = DateTime.parse("20500101T000000")
-
-puts aDateTime
