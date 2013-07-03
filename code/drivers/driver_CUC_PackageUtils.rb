f87806c06defda67129af249ada16d504f534483
borja <borja.lopez.fernandez@gmail.com>
2013-07-03 20:28:52 +0200

    new unit test for compression with 7z

--- code/drivers/driver_CUC_PackageUtils.rb
+++ code/drivers/driver_CUC_PackageUtils.rb
@@ -16,4 +16,7 @@ require 'cuc/PackageUtils'
 
 include CUC::PackageUtils
 
-packZIP("/home/projects/dccdev/local/code/ddc/drivers/file1", true)
+# packZIP("/home/projects/dccdev/local/code/ddc/drivers/file1", true)
+
+puts  pack7z("/home/meteo/Projects/weather/data/archive/minarc_root/REALTIME_XML/CASALE/2013/06/01",
+      "test.7z", false, true)
