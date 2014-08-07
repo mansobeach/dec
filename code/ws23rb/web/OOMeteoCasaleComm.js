25ff56ec26411717a7f1afafaf656c46d2b89ad9
Borja Lopez Fernandez <borja.lopez.fernandez@gmail.com>
2014-08-07 20:53:53 +0200

    change style dynamically for divMeteoTemp document element

--- code/ws23rb/web/OOMeteoCasaleComm.js
+++ code/ws23rb/web/OOMeteoCasaleComm.js
@@ -2,13 +2,12 @@
     
 #########################################################################
 #
-# ===       
+# === 20140716 
 #
 # === Written by Borja Lopez Fernandez
 #
 # === Casale & Beach
 # 
-#
 #
 #########################################################################
 
@@ -16,7 +15,6 @@
 /**
    Class MeteoCasaleComm Function parseMeteoData
 */
-
 
 function MeteoCasaleComm() 
 {
@@ -32,21 +30,17 @@ function MeteoCasaleComm()
 */
 
 MeteoCasaleComm.prototype.parseMeteoData = function(xmlResp)
-{ 
-   // console.debug("parseMeteoData() entry") ;
-   
-   // console.debug(xmlResp) ;
-
-   var xml           = $jQuery.parseXML(xmlResp) ;
-   var date          = $jQuery(xml).find('Date').first().text() ;
-   var aTime         = $jQuery(xml).find('Time').first().text() ;
-   var temp          = $jQuery(xml).find('Temperature').find('Outdoor').find('Value').text() ;
-   var windspeed     = $jQuery(xml).find('Wind').find('Value').text() ;
-   var winddirection = $jQuery(xml).find('Wind').find('Direction').find('Text').text() ;
-   var humidity      = $jQuery(xml).find('Humidity').find('Outdoor').find('Value').text() ;
-   var rain1h        = $jQuery(xml).find('Rain').find('OneHour').find('Value').text() ;
-   var rain24h       = $jQuery(xml).find('Rain').find('TwentyFourHour').find('Value').text() ;
-   var pressure      = $jQuery(xml).find('Pressure').find('Value').text() ;
+{   
+   var xml           = $.parseXML(xmlResp) ;
+   var date          = $(xml).find('Date').first().text() ;
+   var aTime         = $(xml).find('Time').first().text() ;
+   var temp          = $(xml).find('Temperature').find('Outdoor').find('Value').text() ;
+   var windspeed     = $(xml).find('Wind').find('Value').text() ;
+   var winddirection = $(xml).find('Wind').find('Direction').find('Text').text() ;
+   var humidity      = $(xml).find('Humidity').find('Outdoor').find('Value').text() ;
+   var rain1h        = $(xml).find('Rain').find('OneHour').find('Value').text() ;
+   var rain24h       = $(xml).find('Rain').find('TwentyFourHour').find('Value').text() ;
+   var pressure      = $(xml).find('Pressure').find('Value').text() ;
 
    // --------------------------------------------
    // If file content is empty
@@ -70,6 +64,14 @@ MeteoCasaleComm.prototype.parseMeteoData = function(xmlResp)
    aMeteoGauge.rain1h         = rain1h ;
    aMeteoGauge.rain24h        = rain24h ;
 
+   // Classify DOM element divMeteoTemp for CSS format
+
+   // console.debug("pedo") ;
+
+   this.classFormatTemperature(temp) ;
+
+   // console.debug("pis") ;
+
    // add units
    temp        = temp + " \u00BA" + "C" ;
    windspeed   = windspeed + " Km/h" ;
@@ -78,11 +80,7 @@ MeteoCasaleComm.prototype.parseMeteoData = function(xmlResp)
    rain24h     = rain24h + " mm/24h" ;
    pressure    = pressure + " hPa" ;
 
-   console.debug("SUPERPEDO") ;
    console.debug(date + " " + aTime + " " + temp) ;
-
-   
-
 
    // ----------------------------------------------------------------
    // Error handing added
@@ -108,8 +106,26 @@ MeteoCasaleComm.prototype.parseMeteoData = function(xmlResp)
 
    try
    {
-      // $jQuery('#divMeteoTemp').text(temp) ;         
-      $jQuery('a#divMeteoTempLink').text(temp) ;
+      // document.getElementById("divMeteoTemp").innerHTML = temp ;
+      // document.getElementById("divMeteoTemp").value = temp
+      
+      // document.getElementById("divMeteoTemp").text = temp
+      
+      // document.getElementById("divMeteoTemp").innerText = temp
+      
+      // $("divMeteoTemp").text = temp
+      
+      var anElem = $('a') ;
+      
+      var aElem = $('a[href$="win_evolution_temperature-outdoor"]');
+      var children = aElem.children();
+
+      aElem.text(temp);
+      // aElem.append(temp); 
+      
+      aElem.append(children); 
+      
+      
    }
    catch(e)
    {
@@ -118,8 +134,7 @@ MeteoCasaleComm.prototype.parseMeteoData = function(xmlResp)
 
    try
    {
-      // document.getElementById("divMeteoWindSpeed").innerHTML = windspeed ;
-      $jQuery('a#divMeteoWindSpeedLink').text(windspeed) ;
+      document.getElementById("divMeteoWindSpeed").innerHTML = windspeed ;
    }
    catch(e)
    {
@@ -128,8 +143,7 @@ MeteoCasaleComm.prototype.parseMeteoData = function(xmlResp)
    
    try
    {
-      // document.getElementById("divMeteoWindDirection").innerHTML = winddirection ;
-      $jQuery('a#divMeteoWindDirectionLink').text(winddirection) ;
+      document.getElementById("divMeteoWindDirection").innerHTML = winddirection ;
    }
    catch(e)
    {
@@ -138,9 +152,7 @@ MeteoCasaleComm.prototype.parseMeteoData = function(xmlResp)
    
    try
    {
-      // document.getElementById("divMeteoHumidity").innerHTML = humidity ;
-      // $jQuery('#divMeteoHumidity').text(humidity) ;         
-      $jQuery('a#divMeteoHumidityLink').text(humidity) ;
+      document.getElementById("divMeteoHumidity").innerHTML = humidity ;
    }
    catch(e)
    {
@@ -149,8 +161,7 @@ MeteoCasaleComm.prototype.parseMeteoData = function(xmlResp)
    
    try
    {
-      // document.getElementById("divMeteoPressure").innerHTML = pressure ;
-      $jQuery('a#divMeteoPressureLink').text(pressure) ;
+      document.getElementById("divMeteoPressure").innerHTML = pressure ;
    }
    catch(e)
    {
@@ -160,7 +171,6 @@ MeteoCasaleComm.prototype.parseMeteoData = function(xmlResp)
    try
    {
       document.getElementById("divMeteoRain1h").innerHTML = rain1h ;
-      $jQuery('a#divMeteoRain1hLink').text(rain1h) ;
    }
    catch(e)
    {
@@ -178,8 +188,6 @@ MeteoCasaleComm.prototype.parseMeteoData = function(xmlResp)
 
    // this.arrayMeteoGauges.push(aMeteoGauge) ;
 
-
-   // console.debug("parseMeteoData() exit") ;
    return aMeteoGauge ;
 }
 
@@ -192,31 +200,11 @@ MeteoCasaleComm.prototype.parseMeteoData = function(xmlResp)
    
 MeteoCasaleComm.prototype.retrieveMeteoData = function()    
 {
-   // console.debug("retrieveMeteoData() entry") ;
- 
-   new Ajax.Request('/METEO_LEON.xml', {
-      method:'get',
-      onSuccess: function(transport) {
-         MeteoCasaleComm.prototype.parseMeteoData(transport.responseText) ;
-         console.debug("$jQuery('#ul').listview('refresh')") ;
-         $jQuery('#ul').listview('refresh') ;
-      },
-      onFailure: function() { console.error("Error when retrieving METEO_LEON.xml") ; }
-   });
-
-   // console.debug("retrieveMeteoData() exit") ;
-   return ;
-
-}
-//==============================================================================
-
-MeteoCasaleComm.prototype.retrieveMeteoData22 = function()    
-{
    var xmlhttp    = new XMLHttpRequest() ;
    var xmlResp    = null ;
 
-   // xmlhttp.open("GET", "METEO_CASALE.xml", true) ;
-   xmlhttp.open("GET", "METEO_LEON.xml", true) ;
+   xmlhttp.open("GET", "METEO_CASALE.xml", true) ;
+   // xmlhttp.open("GET", "METEO_LEON.xml", true) ;
 
    xmlhttp.onreadystatechange=function()
    {  
@@ -236,4 +224,78 @@ MeteoCasaleComm.prototype.retrieveMeteoData22 = function()
    xmlhttp.send(null) ;
 }
 
+//==============================================================================
+
+/**
+   Function format   
+*/
+   
+MeteoCasaleComm.prototype.classFormatTemperature = function(temperature)    
+{
+   var VHot    = 40,
+       Hot     = 30,
+       VWarm   = 25,
+       Warm    = 20,
+       Cold    = 0 ;
+
+   iTemp = parseInt(temperature) ;
+
+   console.debug(iTemp) ;
+   console.debug(document.getElementById("divMeteoTemp").className) ;
+
+   document.getElementById("divMeteoTemp").className.replace("TemperatureVeryHot", "") ;
+   document.getElementById("divMeteoTemp").className.replace("TemperatureHot", "") ;
+   document.getElementById("divMeteoTemp").className.replace("TemperatureVeryWarm", "") ;
+   document.getElementById("divMeteoTemp").className.replace("TemperatureWarm", "") ;
+   
+   document.getElementById("divMeteoTemp").classList.remove("TemperatureWarm") ;
+   document.getElementById("divMeteoTemp").classList.remove("TemperatureVeryWarm") ;
+   
+   console.debug(document.getElementById("divMeteoTemp").className) ;
+   
+    
+   if (iTemp >= VHot)
+   {
+      console.debug("very hot") ;
+
+      if (document.getElementById("divMeteoTemp").className.indexOf("TemperatureVeryHot") == -1)
+      {
+         document.getElementById("divMeteoTemp").className += " TemperatureVeryHot" ;
+         $("#divMeteoTemp").css({"color":"red"}) ;
+      }
+   }
+   else if(iTemp >= Hot)
+   {
+      console.debug("hot") ;
+
+      if (document.getElementById("divMeteoTemp").className.indexOf("TemperatureHot") == -1)
+      {
+         document.getElementById("divMeteoTemp").className += " TemperatureHot" ;
+         $("#divMeteoTemp").css({"color":"red"}) ;
+      }
+   } 
+   else if(iTemp >= VWarm)
+   {
+      console.debug("very warm") ;
+
+      if (document.getElementById("divMeteoTemp").className.indexOf("TemperatureVeryWarm") == -1)
+      {
+         document.getElementById("divMeteoTemp").className += " TemperatureVeryWarm" ;
+         $("#divMeteoTemp").css({"color":"orangered"}) ;
+      }
+   } 
+   else if(iTemp >= Warm)
+   {
+      console.debug("warm") ;
+
+      if (document.getElementById("divMeteoTemp").className.indexOf("TemperatureWarm") == -1)
+      {
+         document.getElementById("divMeteoTemp").className += " TemperatureWarm" ;
+         $("#divMeteoTemp").css({"color":"orange"}) ;
+      }
+   } 
+
+   console.debug(document.getElementById("divMeteoTemp").className) ;
+
+}
 //==============================================================================
