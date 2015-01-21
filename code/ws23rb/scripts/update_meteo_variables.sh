11f5b6edd4d1c016b5405cf96d6668d0e3a967a9
Borja Lopez Fernandez <borja.lopez.fernandez@gmail.com>
2015-01-21 14:00:19 +0100

    added rain_24hour variable

--- code/ws23rb/scripts/update_meteo_variables.sh
+++ code/ws23rb/scripts/update_meteo_variables.sh
@@ -5,8 +5,8 @@ URL_METEO="meteomonteporzio.altervista.org/update_daily_values.php?variable="
 echo
 echo "Casale & Beach - Meteo Casale update variables"
 echo
-
-for VARIABLE in "temperature_outdoor" "humidity_outdoor" "pressure" "wind_speed" "wind_direction" "rain_1hour"
+ 
+for VARIABLE in "temperature_outdoor" "humidity_outdoor" "pressure" "wind_speed" "wind_direction" "rain_1hour" "rain_24hour"
 do
    echo "curl --silent $URL_METEO$VARIABLE"
    curl --silent $URL_METEO$VARIABLE
