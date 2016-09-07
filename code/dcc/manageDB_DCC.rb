73a35c79374b628fa5db2c2b78703aed3b33af13
Borja Lopez Fernandez <borja.lopez.fernandez@gmail.com>
2016-09-07 12:55:04 +0200

    show of db environment variables

--- code/dcc/manageDB_DCC.rb
+++ code/dcc/manageDB_DCC.rb
@@ -5,22 +5,25 @@
 # manageDB.rb --create-tables | --drop-tables
 #     --create-tables   create all minarc required tables
 #     --drop-tables     drops all minarc tables
+#     --Debug     shows Debug info during the execution
 #     --help   shows this help
 # 
 # == Author
-# DEIMOS-Space S.L.
+# Borja Lopez Fernandez
 #
 # == Copyright
-# Copyright (c) 2013 Casale & Beach
+# Copyleft (c) 2013 Casale & Beach
 #
 
 require 'getoptlong'
-require 'rdoc/usage'
+# require 'rdoc/usage'
 
-require "rubygems"
-require "active_record"
+require 'rubygems'
+require 'active_record'
 
 require 'dcc/DCC_Migrations'
+
+@isDebugMode = false
 
 dbAdapter   = ENV['DCC_DB_ADAPTER']
 dbName      = ENV['DCC_DATABASE_NAME']
@@ -35,21 +38,28 @@ ActiveRecord::Base.establish_connection(:adapter => dbAdapter,
 # MAIN script function
 def main
 
+   dbAdapter   = ENV['DCC_DB_ADAPTER']
+   dbName      = ENV['DCC_DATABASE_NAME']
+   dbUser      = ENV['DCC_DATABASE_USER']
+   dbPass      = ENV['DCC_DATABASE_PASSWORD']
+
    bUp   = false
    bDown = false
    
    opts = GetoptLong.new(
      ["--drop-tables",   "-d",    GetoptLong::NO_ARGUMENT],
      ["--create-tables", "-c",    GetoptLong::NO_ARGUMENT],
+     ["--Debug", "-D",            GetoptLong::NO_ARGUMENT],
      ["--help", "-h",             GetoptLong::NO_ARGUMENT]
      )
     
    begin
       opts.each do |opt, arg|
-         case opt      
-            when "--create-tables"    then @bUp   = true
-            when "--drop-tables"      then @bDown = true
-			   when "--help"             then RDoc::usage
+         case opt
+            when "--Debug"             then @isDebugMode = true      
+            when "--create-tables"     then @bUp   = true
+            when "--drop-tables"       then @bDown = true
+			   when "--help"              then usage
          end
       end
    rescue Exception => e
@@ -58,10 +68,20 @@ def main
    end
 
    if @bDown and @bUp then
-      RDoc::usage("usage")
+      usage
+   end
+
+   if @isDebugMode == true then
+      puts "----------------"
+      puts dbAdapter
+      puts dbName
+      puts dbUser
+      puts dbPass
+      puts "----------------"
    end
 
    if @bDown then
+      CreateReceivedFiles.down
       CreateTrackedFiles.down
       CreateInterfaces.down
       exit(0)
@@ -70,14 +90,33 @@ def main
    if @bUp then
       CreateInterfaces.up
       CreateTrackedFiles.up
+      CreateReceivedFiles.up
       exit(0)
    end
 
-   RDoc::usage("usage")
+   usage
  
    exit(0)
 
 end
+
+#-------------------------------------------------------------
+
+# Print command line help
+def usage
+   fullpathFile = `which #{File.basename($0)}`    
+   
+   value = `#{"head -15 #{fullpathFile}"}`
+      
+   value.lines.drop(1).each{
+      |line|
+      len = line.length - 1
+      puts line[2, len]
+   }
+   exit   
+end
+#-------------------------------------------------------------
+
 
 #=====================================================================
 # Start of the main body
