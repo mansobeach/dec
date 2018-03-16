486033fe8ba57c84db7881e48feb800727659b41
Borja Lopez Fernandez <borja.lopez.fernandez@gmail.com>
2018-03-16 17:12:27 +0100

    update to the testing environment

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo add-apt-repository "deb https://apt.postgresql.org/pub/repos/apt/ precise-pgdg main"
+
 sudo apt-get -y update
 
 sudo apt-get -y install software-properties-common
@@ -14,13 +16,20 @@ sudo apt-get -y update
 
 sudo apt-get -y install curl
 
-sudo apt-get -y install ruby2.4 ruby2.4-dev
+sudo apt-get -y install p7zip
+
+sudo apt-get -y install p7zip-full
 
 sudo apt-get -y install sqlite3  libsqlite3-dev
 
-sudo apt-get -y install postgresql
+sudo apt-get -y install postgresql-9.4
+# sudo apt-get -y install postgresql
 
-sudo gem update --system  2.7.4
+# It is recommended to install ruby locally with rvm
+#
+# sudo apt-get -y install ruby2.4 ruby2.4-dev
+
+# sudo gem update --system  2.7.4
 # sudo gem update --system
 
-sudo gem install bundler
+# sudo gem install bundler
