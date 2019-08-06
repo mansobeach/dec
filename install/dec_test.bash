836e0901c6222d6d753a725bf29dee7affc6c52f
bolf <borja.lopez.fernandez@gmail.com>
2019-08-06 19:16:43 +0200

    DEC test environment updated

--- install/dec_test.bash
+++ install/dec_test.bash
@@ -1,5 +1,5 @@
 export DEC_DB_ADAPTER=sqlite3
-export DEC_DATABASE_NAME=$HOME/Sandbox/inventory/dec_inventory
+export DEC_DATABASE_NAME=/tmp/dec_inventory
 export DEC_DATABASE_USER=root
 export DEC_DATABASE_PASSWORD=1mysql
 export DEC_CONFIG=$HOME/Projects/dec/config
