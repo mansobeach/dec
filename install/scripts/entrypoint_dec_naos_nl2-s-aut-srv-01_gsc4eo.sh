5e0983e00bfce6599598f991e4d7715f33ddae13
Borja Lopez Fernandez <borja.lopez@deimos-space.com>
2023-05-04 12:31:41 +0200

    DEC 1.0.39.6 / configuration for system tests in NAOS

--- install/scripts/entrypoint_dec_naos_nl2-s-aut-srv-01_gsc4eo.sh
+++ install/scripts/entrypoint_dec_naos_nl2-s-aut-srv-01_gsc4eo.sh
@@ -18,6 +18,8 @@ decListener -m SIM_KSAT_TLM -i 60
 sleep 10
 decListener -m SIM_KSAT_SCH_RPL -i 60
 sleep 10
+decListener -m MCS_EXT_RPT_EOT -i 300
+sleep 10
 echo "container entrypoint started DEC NAOS gsc4eo@nl2-s-aut-srv-01"
 
 touch /tmp/foo.txt
--- install/scripts/entrypoint_dec_naos_nl2-u-moc-srv-01_gsc4eo.sh
+++ install/scripts/entrypoint_dec_naos_nl2-u-moc-srv-01_gsc4eo.sh
@@ -10,6 +10,10 @@ echo "container entrypoint init DEC NAOS gsc4eo@nl2-s-aut-srv-01"
 # decListener -m TEST_NASA_NBULA -i 86400
 # decListener -m TEST_NASA_NBULC -i 86400
 # decListener -m TEST_NASA_SFL -i 86400
+# decListener -m TEST_ECMWF_TCC -i 3600
+
+decListener -m ECMWF_TCC -i 3600
+sleep 10
 decListener -m CELESTRAK_SFS -i 86400
 sleep 10
 decListener -m CELESTRAK_TLE -i 86400
